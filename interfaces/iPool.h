c124b7aa0bdd3684d8f42bb635dec2d058f82df7
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:37:38 -0400

    first commit

---
+++ interfaces/iPool.h
@@ -0,0 +1,15 @@
+#ifndef IPOOL_H
+#define IPOOL_H
+
+#include "iPoolItem.h"
+
+class iPool
+{
+public:
+    virtual ~iPool() {}
+    virtual iPoolItem* aquire() = 0;
+    virtual void release(iPoolItem* item) = 0;
+
+};
+
+#endif // IPOOL_H
